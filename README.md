e381efaf86b64d55371e00a294a4749c37ee863a
jimi-chhatrala <jimichhatrala8488@gmail.com>
2024-07-29 00:33:51 +0530

    Creating GraphQL Schema Completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Expense Tracker GraphQL
+
+A full-stack expense tracker app with Express, MongoDB, React, and Apollo.
--- backend/typeDefs/transaction.typeDef.js
+++ backend/typeDefs/transaction.typeDef.js
@@ -1,5 +1,46 @@
 import { gql } from "apollo-server-express";
 
-const transactionTypeDef = gql``;
+const transactionTypeDef = gql`
+  type Transaction {
+    _id: ID!
+    userId: ID!
+    description: String!
+    paymentType: String!
+    category: String!
+    amount: Float!
+    location: String
+    date: String!
+  }
+
+  type Query {
+    transactions: [Transaction!]
+    transaction(transactionId: ID!): Transaction
+  }
+
+  type Mutation {
+    createTransaction(input: CreateTransactionInput!): Transaction!
+    updateTransaction(input: UpdateTransactionInput!): Transaction!
+    deleteTransaction(transactionId: ID!): Transaction!
+  }
+
+  input CreateTransactionInput {
+    description: String!
+    paymentType: String!
+    category: String!
+    amount: Float!
+    date: String!
+    location: String
+  }
+
+  input UpdateTransactionInput {
+    transactionId: ID!
+    description: String
+    paymentType: String
+    category: String
+    amount: Float
+    location: String
+    date: String
+  }
+`;
 
 export default transactionTypeDef;
